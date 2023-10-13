ab7fb3526297fd012672248200a7302cbb12ce62
Cyber <tessa.brzyk@icloud.com>
2023-10-13 18:23:50 +0200

    oef

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
 <script>
 
+export default{
+  data(){
+    return{
+      naam: "Tessa",
+      achternaam: "Brzyk",
+      url: "https//:google.com",
+      number: 0,
+    }
+  },
+  methods:{
+    showFullName(){
+      return "Tessa Brzyk";
+    },
+    showName(){
+      this.naam = this.naam + "Brzyk";
+      return this.naam;
+    },
+    addNumber(nr){ //verwacht dat getal wordt meeggegeven
+      this.number = this.number + nr;  //getal wordt hiermee opgeteld ()
+    }
+  },
+}
 </script>
 
 <template>
-  <h1>Welkom in de Vue App</h1>
+  
+  <h1>Welkom in het project</h1>
+  <p>{{ naam }}</p>
+  <h2>{{ showFullName() }}</h2>
+  <p>{{ showName }}</p>
+  <a :href="url">google</a><br>
+
+
+  <p>{{ number }}</p>
+  <button @:click="addNumber(0)">Click</button>  <!-- addNumber(11): on click 11 + 11 ....-->
+
+
+
+
+
 </template>
 
 <style>
